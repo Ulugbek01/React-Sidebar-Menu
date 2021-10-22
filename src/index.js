a00ec5aa5240456f2cde35361f74e0641b6a9693
Ulugbek01 <xyz207820@gmail.com>
2021-10-22 19:04:22 +0500

    product item add

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import NavItem from "./NavItem";
+import ProductItem from "./ProductItem";
 
 ReactDOM.render(
   <React.StrictMode>
     <NavItem />
+    <ProductItem />
   </React.StrictMode>,
   document.getElementById("root")
 );
